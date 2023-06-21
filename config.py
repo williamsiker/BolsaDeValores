15665563de693854ecd40b6ed3af49988e753d79
Williams Iker Luque Portada <105296030+williamsiker@users.noreply.github.com>
2023-06-21 11:38:25 -0500

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class DevelopmentConfig():
     MYSQL_USER = 'u7mh57g1lixv0lo1'
     MYSQL_PASSWORD = 'FZLTCB579yBQMv1fGGuo'
     MYSQL_DB = 'bxk4cskcwqztbxam1yvk'
+    SECRET_KEY = 'B!1w8NAt1T^%kvhUI*S^'
 
 config={
     'development':DevelopmentConfig
