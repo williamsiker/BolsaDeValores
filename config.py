c1028b981d7a702282af0c808cc5faeafeb52b47
Williams Iker Luque Portada <105296030+williamsiker@users.noreply.github.com>
2023-06-21 12:15:54 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
-class Config:
-    SECRET_KEY = 'B!1w8NAt1T^%kvhUI*S^'
-
 class DevelopmentConfig(Config):
     DEBUG = True
     MYSQL_HOST = 'bxk4cskcwqztbxam1yvk-mysql.services.clever-cloud.com'
     MYSQL_USER = 'u7mh57g1lixv0lo1'
     MYSQL_PASSWORD = 'FZLTCB579yBQMv1fGGuo'
     MYSQL_DB = 'bxk4cskcwqztbxam1yvk'
+    SECRET_KEY = 'B!1w8NAt1T^%kvhUI*S^'
 
 config={
     'development':DevelopmentConfig
